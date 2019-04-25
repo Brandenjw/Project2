e80e7a0aa0dae24847014bfa4697d3f0d441f381
Branden White <Brandenjwhite@gmail.com>
2019-04-25 13:27:16 -0400

    adding const to server

--- server.js
+++ server.js
@@ -1,3 +1,21 @@
 const express = require('express')
 const app = express()
 const methodOverride = require('method-override')
+
+const userApi = require('./api/userApi.js');
+const accountApi = require('./api')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
