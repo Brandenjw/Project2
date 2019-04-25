26ef9a616dd99a9d2d7cb4f05e840e3335630ead
Branden White <Brandenjwhite@gmail.com>
2019-04-25 13:34:27 -0400

    added views folder and adjusted server

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
-const express = require('express')
-const app = express()
-const methodOverride = require('method-override')
 
-const userApi = require('./api/userApi.js');
-const accountApi = require('./api')
+const express = require('express');
+const app = express();
+const methodOverride = require('method-override');
 
 
+// const userApi = require('./api/userApi.js');
+// const accountApi = require('./api/waysApi.js');
+//sets up hbs
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname+"/public"));
+//setup middleware for handling html forms
+//where body is a query string 
+app.use(express.urlencoded());
+app.use(methodOverride('_method'))
+
+app.get('/', (req, res) => {
+    res.render("user")
+   });
 
 
 
@@ -17,5 +28,8 @@ const accountApi = require('./api')
 
 
 
-
+//
 const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log("server has started");
+});
