8c685643961d4e8e44a4099f4f903c9d9b42bfb2
Branden White <Brandenjwhite@gmail.com>
2019-04-30 09:44:09 -0400

    adjusted routes/index

--- Routes/index.js
+++ Routes/index.js
@@ -1,6 +1,43 @@
 let express = require('express')
 let router = express.Router()
-const donutController = require('../controllers/donuts.js')
+const userController = require('../Controllers/user.js')
 
-//GET index Route
-router.get('/',donutController.index)
+
+router.get("/", (req, res) => {
+
+  res.render("landing")
+})
+
+router.get("/new", userController.new)
+
+
+//Sends back a single page with the details of a single account displayed
+// router.get("/index/:id", (req, res) => {
+//     res.render("index/", { user } ); 
+//   });
+
+//   router.get('/index/new'), (req, res) => {
+//     res.render('user/new')
+//   };
+ 
+  
+  //Users POST
+  //this should add a new POST from req.body to users (global variable)
+  //and sends back the same page to list all accounts.
+  router.post("/", userController.create)
+  
+  //Users PUT (note here you'll need to put /put at the end of your
+  //path. This is a work around because HTML forms only allow GET and POST
+  //requests). Make sure the HTML Form has the pattern: action='.../put' 
+  // router.put("users/:id/put", (req, res) => {
+  // });
+  
+  //accounts DELETE (note here you'll need to put /delete at the end of your
+  //path. This is a work around because HTML forms only allow GET and POST
+  //requests). Make sure the HTML Form has the pattern: action='.../put' 
+  // router.delete("/users/:id/delete", (req, res) => { 
+  // });
+  
+
+
+module.exports = router
